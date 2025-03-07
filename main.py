641ac7e8a05406e67f26e47fe17183d6ccf0a336
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-07 16:35:13 -0500

    Added clock to reduce game speed to a specific framerate and delta time for later use

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import constants as ct
 def main():
     """Runs main game loop"""
     screen = pygame.display.set_mode((ct.SCREEN_WIDTH, ct.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
