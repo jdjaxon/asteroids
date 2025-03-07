fc5ff309a4c2da45cff83dc25d1423a12ffdac19
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-07 16:35:56 -0500

    Added module with class defining circle game sprites

---
+++ circleshape.py
@@ -0,0 +1,25 @@
+"""Module for Circle game Sprites"""
+
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    """Class for Circle game Sprites"""
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        """Draw the Sprite to the screen. Will be overridden by sub-classes"""
+        pass
+
+    def update(self, dt):
+        """Update the Sprite. Will be overridden by sub-classes"""
+        pass
