86c35ce030b42c5cfb33f80a509e5ad7f8d7b72e
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-03-07 16:22:14 -0500

    Setting screen fill color with simpler string instead of RGBA code

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ import constants as ct
 def main():
     """Runs main game loop"""
     screen = pygame.display.set_mode((ct.SCREEN_WIDTH, ct.SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        screen.fill("black")
         pygame.display.flip()
 
 
